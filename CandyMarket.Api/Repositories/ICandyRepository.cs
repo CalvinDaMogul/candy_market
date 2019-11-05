44f0b6f31fea7653e9de5ffdc63a77366f66460d
Calvin Foster <cal.j.foster@gmail.com>
2019-11-04 22:44:56 -0600

    new commit

--- CandyMarket.Api/Repositories/ICandyRepository.cs
+++ CandyMarket.Api/Repositories/ICandyRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using CandyMarket.Api.DataModels;
 using CandyMarket.Api.Dtos;
 
+
 namespace CandyMarket.Api.Repositories
 {
     public interface ICandyRepository
     {
+
         IEnumerable<Candy> GetAllCandy();
         bool AddCandy(AddCandyDto newCandy);
         bool EatCandy(Guid candyIdToDelete);
